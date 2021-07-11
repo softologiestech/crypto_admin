c308c2575b2606b3f69d51614b3330f1c464ffa5
theprogramerdude <shas19980805@gmail.com>
2021-07-11 19:06:06 +0530

    crypto admin finalized

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCs96Q8tJd10S1le-zd3d1eUo1HXych-zU',
+    authDomain: 'crypto-cbd23.firebaseapp.com',
+    projectId: 'crypto-cbd23',
+    storageBucket: 'crypto-cbd23.appspot.com',
+    messagingSenderId: '656783905714',
+    appId: '1:656783905714:web:facccd4aa0962eee7b8aab',
+    measurementId: 'G-NPH2JDGVLH',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCs96Q8tJd10S1le-zd3d1eUo1HXych-zU',
+    authDomain: 'crypto-cbd23.firebaseapp.com',
+    projectId: 'crypto-cbd23',
+    storageBucket: 'crypto-cbd23.appspot.com',
+    messagingSenderId: '656783905714',
+    appId: '1:656783905714:web:facccd4aa0962eee7b8aab',
+    measurementId: 'G-NPH2JDGVLH',
+  },
+  // wsUri: 'ws://test.lisuns.com:4575',
+  // password: '7de118a2-062d-43c5-a3fc-875d52cdd9f9',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
